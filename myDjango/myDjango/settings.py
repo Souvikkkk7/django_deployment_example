063c196498c7f43b6b74e1cd9df53dd6f24f195e
Souvikkkk7 <66178067+Souvikkkk7@users.noreply.github.com>
2020-06-29 01:44:22 +0530

    Update settings.py

--- myDjango/myDjango/settings.py
+++ myDjango/myDjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '+y(u24yj_-nqvnu=4j&*41nhb33o)p^_jv3y&d13)4v2_x&8ds'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['souvikkkk.pythonanywhere.com']
 
 
 # Application definition
